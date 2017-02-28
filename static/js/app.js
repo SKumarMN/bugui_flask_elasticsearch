18f5a451c7ca546e4dd6c7e790486536b974ae63
SKumarMN <sharathkumarmn@gmail.com>
2017-02-28 13:16:21 -0700

    first working version

--- static/js/app.js
+++ static/js/app.js
@@ -3,14 +3,14 @@ app.controller('formController', function($scope, $http, $sce) {
 
     $scope.searchResults = [];
     $scope.bugInfo = {};
-    $scope.buttonClicked = function() {
-
-
-
-        //$scope.modText = "Hello "+$scope.inputText;
-    };
 
     $scope.search = function() {
+        $scope.searchFlag = false;
+        searchHelper();
+
+    };
+
+    searchHelper = function() {
         if ($scope.searchText && ($scope.searchText).length > 3) {
             $scope.searchResults = [];
             var dataObj = {
@@ -37,11 +37,42 @@ app.controller('formController', function($scope, $http, $sce) {
             }, function errorCallback(response) {
                 console.log(response.statusText);
             });
+        }else{
+             $scope.searchResults = [];
         }
+    }
+    $scope.searchAll = function() {
+        $scope.searchFlag = true;
+        searchAllHelper();
 
     };
+    searchAllHelper = function() {
+        $scope.searchResults = [];
+        var dataObj = {
+            text: $scope.searchText,
+            tags: $scope.tags
+        };
+        $http({
+            method: 'POST',
+            url: 'searchAll',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(dataObj)
+        }).then(function successCallback(response) {
+            $scope.searchResults = [];
+            hits = response.data.hits.hits
+            for (var i = 0; i < hits.length; i++) {
+                $scope.searchResults.push({
+                    "id": hits[i]["_source"]["RPTNO"],
+                    "subject": $sce.trustAsHtml(hits[i]["_source"]["SUBJECT"])
+                })
+            }
 
-
+        }, function errorCallback(response) {
+            console.log(response.statusText);
+        });
+    }
     $scope.getBugDetails = function(bugId) {
 
         $http({
@@ -58,11 +89,19 @@ app.controller('formController', function($scope, $http, $sce) {
     $scope.tags = [
 
     ];
+    $scope.searchText = "";
+
     $scope.tagAdded = function(tag) {
-        console.log('Tag added: ', tag);
+        if ($scope.searchFlag)
+            searchAllHelper();
+        else
+            searchHelper();
     };
     $scope.tagRemoved = function(tag) {
-        console.log('Tag removed: ', tag);
+        if ($scope.searchFlag)
+            searchAllHelper();
+        else
+            searchHelper();
     };
 
     $scope.loadTags = function(query) {
--- webapp.py
+++ webapp.py
@@ -4,6 +4,9 @@ from elasticsearch import Elasticsearch
 app = Flask(__name__)
 app.debug = True
 client = Elasticsearch()
+ES_BUG_INDEX = "bugdb"
+
+tags_dict = {"ASSIGNED_TO": "PROGRAMMER", "COMPONENT": "CATEGORY", "STATUS": "STATUS", "RPTD_BY": "RPTD_BY"}
 
 
 @app.route('/')
@@ -15,16 +18,25 @@ def index():
 def api_search():
     if request.headers['Content-Type'] == 'application/json':
         data = request.json
-        print(data)
-        return json.dumps(getNItems(data['text']))
+
+        return json.dumps(getNItems(data))
+    return "No Data"
+
+
+@app.route('/searchAll', methods=['POST'])
+def api_search_all():
+    if request.headers['Content-Type'] == 'application/json':
+        data = request.json
+        # print(data)
+        return json.dumps(getMatchingBugs(data))
     return "No Data"
 
 
 @app.route('/suggest')
 def api_suggest():
-    print("here")
+    # print("here")
     if 'query' in request.args:
-        print(request.args['query'])
+        # print(request.args['query'])
         return json.dumps(getSuggestItems(request.args['query']))
     else:
         return 'Nothing to Query'
@@ -33,7 +45,7 @@ def api_suggest():
 @app.route('/bug/<bugid>')
 def api_bug(bugid):
     # print(bugid)
-    response = client.get(index="bugdb2", doc_type='bugdb', id=bugid)
+    response = client.get(index=ES_BUG_INDEX, doc_type='bugdb', id=bugid)
     # print(response)
     return json.dumps(response["_source"])
 
@@ -56,23 +68,110 @@ def getSuggestItems(text):
     )
 
     return response['suggest']['tag-suggest'][0]['options']
-    # for hit in response['hits']['hits']:
-    #   print(hit['_score'], hit['_source']['SUBJECT'])
 
 
-def getNItems(text):
+def getMatchingBugs(data):
+    response = ""
+    filter = []
+    query = {"size": 50, "_source": ["SUBJECT", "RPTNO"], "query": {"bool": {}},
+             "highlight": {"pre_tags": ["<b>"], "post_tags": ["</b>"],
+                           "fields": {"SUBJECT.simple": {}, "COMMENTS.MESSAGE": {}}}}
+
+    filter = getFilters(data)
+
+    should_clause =[{
+               "match":{
+                  "SUBJECT.simple":{
+                     "query":data["text"],
+                      "operator":"and"
+                  }
+               }
+            },
+            {
+               "nested":{
+                  "path":"COMMENTS",
+                  "score_mode":"max",
+                  "query":{
+                     "bool":{
+                        "must":{
+                           "match":{
+                              "COMMENTS.MESSAGE":{
+                                 "query":data["text"],
+                                 "operator":"and"
+                              }
+                           }
+                        }
+                     }
+                  }
+               }
+            }]
+
+    if len(filter) > 0 and len(data["text"]) == 0:
+        query["query"]["bool"].update({"should":{"match_all": {}}})
+        query["query"]["bool"].update({"filter": filter})
+    elif len(filter) > 0 and len(data["text"]) > 0:
+        query["query"]["bool"].update({"should":should_clause })
+        query["query"]["bool"].update({"filter": filter})
+    else:
+        query["query"]["bool"].update({"should": should_clause})
     response = client.search(
-        index="bugdb2",
-        body={
-            "_source": ["SUBJECT"],
-            "size": 30,
-            "query": {
-                "match": {
-                    "SUBJECT": {
-                        "query": text,
-                        "operator": "and"
-                    }
+        index=ES_BUG_INDEX,
+        body=json.dumps(query))
+    print(json.dumps(query))
+    return response
 
+
+def getFilters(data):
+    filter = []
+    if len(data["tags"]) > 0:
+        for obj in data["tags"]:
+            tag_list = obj["text"].split(":")
+
+            if len(filter) == 0:
+
+                filter.append({
+                    "terms": {
+                        tags_dict[tag_list[0]]: [
+                            tag_list[1]
+                        ]
+                    }
+                })
+
+            else:
+                flag = False
+
+                for item in filter:
+
+                    if tags_dict[tag_list[0]] in item["terms"]:
+                        item["terms"][tag_list[0]].append(tag_list[1])
+                        flag = True
+                        break
+                if not flag:
+                    filter.append({
+                        "terms": {
+                            tags_dict[tag_list[0]]: [
+                                tag_list[1]
+                            ]
+                        }
+                    })
+    return filter
+
+
+def getNItems(data):
+    query =  {
+            "_source": ["SUBJECT","RPTNO"],
+            "size": 20,
+            "query": {
+                "bool": {
+                    "must": {
+                        "match": {
+                            "SUBJECT": {
+                                "query": data["text"],
+                                "operator": "and"
+                            }
+
+                        }
+                    }
                 }
             },
             "highlight": {
@@ -83,8 +182,16 @@ def getNItems(text):
                 }
             }
         }
+    filter = getFilters(data)
+    if len(data["tags"]) > 0:
+        query["query"]["bool"].update({"filter": filter})
+
+
+    response = client.search(
+        index=ES_BUG_INDEX,
+        body=json.dumps(query)
     )
-    # print(response)
+    print(json.dumps(query))
     return response
     # for hit in response['hits']['hits']:
     #   print(hit['_score'], hit['_source']['SUBJECT'])
@@ -92,3 +199,11 @@ def getNItems(text):
 
 if __name__ == '__main__':
     app.run(host='slc12acw.us.oracle.com', port=9252)
+
+
+"""   query["query"]["bool"]["must"].update({"multi_match": {"query": data["text"], "type": "best_fields",
+                                                               "fields": ["COMMENTS.MESSAGE", "SUBJECT.simple"]}})
+        query["query"]["bool"].update({"filter": filter})
+    else:
+        query["query"]["bool"]["must"].update({"multi_match": {"query": data["text"], "type": "best_fields",
+                                                               "fields": ["COMMENTS.MESSAGE", "SUBJECT.simple"]}})"""
